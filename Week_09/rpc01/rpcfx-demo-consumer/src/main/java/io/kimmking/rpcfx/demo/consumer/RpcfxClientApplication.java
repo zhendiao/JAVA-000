479a417b48f908338f5bb34de376651fcdd730ef
zhendiao <351848327@qq.com>
2020-12-16 00:03:42 +0800

    增加调用代码

--- Week_09/rpc01/rpcfx-demo-consumer/src/main/java/io/kimmking/rpcfx/demo/consumer/RpcfxClientApplication.java
+++ Week_09/rpc01/rpcfx-demo-consumer/src/main/java/io/kimmking/rpcfx/demo/consumer/RpcfxClientApplication.java
@@ -1,12 +1,20 @@
 package io.kimmking.rpcfx.demo.consumer;
 
 import io.kimmking.rpcfx.api.RpcInvoker;
+import io.kimmking.rpcfx.api.RpcfxRequest;
+import io.kimmking.rpcfx.api.RpcfxResolver;
+import io.kimmking.rpcfx.api.RpcfxResponse;
 import io.kimmking.rpcfx.client.Rpcfx;
 import io.kimmking.rpcfx.demo.api.Order;
 import io.kimmking.rpcfx.demo.api.OrderService;
 import io.kimmking.rpcfx.demo.api.User;
 import io.kimmking.rpcfx.demo.api.UserService;
+import io.kimmking.rpcfx.server.RpcfxInvoker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class RpcfxClientApplication {
@@ -15,12 +23,26 @@ public class RpcfxClientApplication {
 	// 三方库 lib
 	// nexus, userserivce -> userdao -> user
 	//
+	@Bean
+	public RpcfxInvoker createInvoker(@Autowired RpcfxResolver resolver){
+		return new RpcfxInvoker(resolver);
+	}
+	@Autowired
+	RpcfxInvoker invoker;
 
+
+	@PostMapping("/")
+	public RpcfxResponse invoke(@RequestBody RpcfxRequest request) {
+		return invoker.invoke(request);
+	}
+	@Bean
+	public RpcfxResolver createResolver(){
+		return new DemoResolver();
+	}
 	public static void main(String[] args) {
 
 		// UserService service = new xxx();
 		// service.findById
-
 		UserService userService = Rpcfx.create(UserService.class, "http://localhost:8090/");
 		User user = userService.findById(1);
 		System.out.println("find user id=1 from server: " + user.getName());
